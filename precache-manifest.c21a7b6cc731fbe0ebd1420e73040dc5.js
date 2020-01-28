fd4310f2346cf06237c22abe5810402b71dc658d
wdixon2186 <53952397+wdixon2186@users.noreply.github.com>
2020-01-27 19:31:07 -0700

    Updates

---
+++ precache-manifest.c21a7b6cc731fbe0ebd1420e73040dc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f634fccdcd88578ef7d5ac8041e14d20",
+    "url": "/ReactQuiz/index.html"
+  },
+  {
+    "revision": "843c7e97ddfc4448f426",
+    "url": "/ReactQuiz/static/css/main.05743fa3.chunk.css"
+  },
+  {
+    "revision": "d5eb87836b15bab9fefa",
+    "url": "/ReactQuiz/static/js/2.69a046f5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ReactQuiz/static/js/2.69a046f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "843c7e97ddfc4448f426",
+    "url": "/ReactQuiz/static/js/main.9efb7307.chunk.js"
+  },
+  {
+    "revision": "85855145372ec73e0412",
+    "url": "/ReactQuiz/static/js/runtime-main.226b8ada.js"
+  }
+]);
